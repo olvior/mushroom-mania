451bd852535921f8c37555796f8daba4a114eb17
Oliver Drescher <oliver@someone.lan>
2024-03-30 09:28:20 +0100

    Separated tscn files and scripts

--- leveldesign/parts/exit.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-class_name AreaExit
-
-@export var name_id : StringName
-@export var connects_to_area : StringName
-@export var connects_to_loc : StringName
-@export var spawn_loc : Node2D
--- leveldesign/parts/spawnarea.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-class_name SpawnArea
-
-@export var spawn_point : Node2D
